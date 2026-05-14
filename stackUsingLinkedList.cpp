f9c4f412f6c7329a8b9d80464a0e5368253b4c35
Pis23 <nafisshl10@gmail.com>
2026-05-14 22:48:31 +0700

    isi fungsi push

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -26,6 +26,11 @@ class stack
         }
         int push(int value)
         {
-            
+            Node *newNode = new Node();
+            newNode->data = value;
+            newNode->next = top;
+            top = newNode;
+            cout << "Push Value: " << value << endl;
+            return value; 
         }
 };
