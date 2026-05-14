b47af47ccd884d8fdfdeeb491fc7b9e2f3cb33fc
Pis23 <nafisshl10@gmail.com>
2026-05-14 23:18:06 +0700

    default invalid inout

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -117,5 +117,9 @@ int main()
         case 4:
 			cout << "Exiting program." << endl;
 			break;
+        default:
+			cout << "invalid choice. Try again." << endl;
+			break;
         }
+        cout << endl;
 };
